74bd7e7b94abfca2d75dd8c613ef46b57aea7373
Maurice <maurice@pop-os.localdomain>
2020-11-11 15:41:05 +0100

    test implementation

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+
+app.use(express.static('presultublic'))
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.listen(8080,'127.0.0.1', () => console.log(`Wikipedia-medication-extractor listening at ${'127.0.0.1'}:${8080}`))
